9805db973eb8d167eb4319d04d4b4981e4ea6a19
zhanglei <lei.zhang@saegus.com>
2021-11-30 12:27:05 +0100

    back

--- back-coolie/app.js
+++ back-coolie/app.js
@@ -1,5 +1,12 @@
-const express = requre("express");
+const express = require("express");
 const app = express();
+
+app.use((req, res, next)=>{
+	res.setHeader('Access-Control-Allow-Origin', '*');//允许哪个路径来访问 *代表全部
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');//允许哪个HttpMethod访问
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');//允许在header里加入的参数
+	next();
+})
 
 app.use("/", (req, res, next)=>{
     res.status(200).json({
@@ -8,4 +15,6 @@ app.use("/", (req, res, next)=>{
 })
 
 
-app.listen(process.env.PORT_BACK);
+app.listen(process.env.PORT_BACK, ()=> {
+    console.log("Connected Back")
+});
