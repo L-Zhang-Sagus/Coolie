1138d1cf47ec3667db6cc26c6432e327e4fed5a1
zhanglei <lei.zhang@saegus.com>
2021-11-30 12:14:15 +0100

    back

--- front-coolie/src/app/app.component.ts
+++ front-coolie/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private baseUrl = environment.baseUrl;
+  constructor(
+    private http:HttpClient
+  ){}
+  ngOnInit(){
+    this.http.get(this.baseUrl).subscribe(res=>{
+      console.log(res)
+    })
+  }
   title = 'front-coolie';
 }
