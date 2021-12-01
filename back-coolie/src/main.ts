d9c68eef19ee5f498a59df4ab14f63c0e0b654cb
zhanglei <lei.zhang@saegus.com>
2021-12-01 17:20:22 +0100

    back est teminé

--- back-coolie/src/main.ts
+++ back-coolie/src/main.ts
@@ -6,7 +6,11 @@ import { AppModule } from './app.module';
 let PORT = new ConfigService().get("PORT_BACK", 5555);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors:true});
+  app.use((req, res, next)=>{
+    console.log("Good Test");
+    next()
+  })
   await app.listen(PORT);
 }
 bootstrap();
